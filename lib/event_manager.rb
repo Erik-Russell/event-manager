4c82657b8cfae068294773a0e5fb9dca2f64f471
Erik Russell <erik.z.russell@gmail.com>
2024-09-14 11:05:38 -0700

    added CSV library and exception handler

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,17 @@
 # frozen_string_literal: true
 
-puts 'Event Manager Initialized!'
+require 'csv'
+
+puts ' **EventManager Initialized!**'
+
+file_path = 'event_attendees.csv'
+
+begin
+  contents = CSV.open(file_path, headers: true)
+  contents.each do |row|
+    name = row[2]
+    puts name
+  end
+rescue Errno::ENOENT
+  puts 'ERROR => File does not exist.'
+end
