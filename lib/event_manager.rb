ffd50e42496728925ec0c278df2795feb06c4059
Erik Russell <erik.z.russell@gmail.com>
2024-09-14 11:10:56 -0700

    Iteration 1 complete

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -7,10 +7,15 @@ puts ' **EventManager Initialized!**'
 file_path = 'event_attendees.csv'
 
 begin
-  contents = CSV.open(file_path, headers: true)
+  contents = CSV.open(
+    file_path,
+    headers: true,
+    header_converters: :symbol
+  )
   contents.each do |row|
-    name = row[2]
-    puts name
+    name = row[:first_name]
+    zipcode = row[:zipcode]
+    puts "#{name} #{zipcode}"
   end
 rescue Errno::ENOENT
   puts 'ERROR => File does not exist.'
